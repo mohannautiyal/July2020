5480317ba3e68cc94305e1be887dff0673aac1d9
mohannautiyal <mohannautiyal80@gmail.com>
2020-07-07 22:23:20 +0530

    TestNG Revision

---
+++ TestNgProj/src/main/java/com/home/testNg.java
@@ -0,0 +1,30 @@
+package com.home;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class testNg {
+	
+	
+	@Test(priority=1)
+	public void firstTest() {
+		System.out.println("Running first test");
+		assertTrue(false);
+		
+	}
+	
+	@Test(priority=2,dependsOnMethods= {"firstTest"})
+	public void SecondTest() {
+		System.out.println("Running second test");
+		
+	}
+	
+	
+	@Test(priority=3)
+	public void ThirdTest() {
+		System.out.println("Running third test");
+		
+	}
+
+}
