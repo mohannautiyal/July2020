27aa6bd53785cf02cc920c101fb527244c55481d
mohannautiyal <mohannautiyal80@gmail.com>
2020-07-07 22:30:28 +0530

    ITestContext

--- TestNgProj/src/main/java/com/home/testNg.java
+++ TestNgProj/src/main/java/com/home/testNg.java
@@ -2,6 +2,7 @@ package com.home;
 
 import static org.testng.Assert.assertTrue;
 
+import org.testng.ITestContext;
 import org.testng.annotations.Test;
 
 public class testNg {
@@ -10,21 +11,22 @@ public class testNg {
 	@Test(priority=1)
 	public void firstTest() {
 		System.out.println("Running first test");
-		assertTrue(false);
+		assertTrue(true);
 		
 	}
 	
 	@Test(priority=2,dependsOnMethods= {"firstTest"} )
-	public void SecondTest() {
+	public void SecondTest(ITestContext i) {
 		System.out.println("Running second test");
-		
+		i.setAttribute("TestAttribute",123);
 	}
 	
 	
 	@Test(priority=3)
-	public void ThirdTest() {
+	public void ThirdTest(ITestContext i) {
 		System.out.println("Running third test");
 		
+		System.out.println(i.getAttribute("TestAttribute"));
 	}
 
 }
