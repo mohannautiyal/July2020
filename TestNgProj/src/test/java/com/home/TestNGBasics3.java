69209ef50acc6e43b50f2442d755018c4039a6b8
mohannautiyal <mohannautiyal80@gmail.com>
2020-07-26 22:43:52 +0530

    TestNg.xml configurations

--- TestNgProj/src/test/java/com/home/TestNGBasics3.java
+++ TestNgProj/src/test/java/com/home/TestNGBasics3.java
@@ -2,17 +2,18 @@ package com.home;
 
 import static org.testng.Assert.assertTrue;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestNGBasics3 {
 
 	
-	
+	@Parameters({"param1","param2"})
 	@Test(priority=1)
-	public void Login() {
+	public void Login(String param1,String param2) {
 		
-		System.out.println("Logging into Application");
-		assertTrue(false);
+		System.out.println("Logging into Application with "+param1+" "+param2);
+		assertTrue(true);
 	}
 	
 	
